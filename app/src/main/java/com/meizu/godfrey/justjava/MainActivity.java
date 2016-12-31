ddce0e963d2c1185745cef855d6152970dff4a24
godfrey <wugaofu@gmail.com>
2016-12-31 21:05:58 +0800

    add Chocolate Checkbox

--- app/src/main/java/com/meizu/godfrey/justjava/MainActivity.java
+++ app/src/main/java/com/meizu/godfrey/justjava/MainActivity.java
@@ -19,11 +19,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void  submitOrder(View view){
-        CheckBox checkBox=(CheckBox)findViewById(R.id.checkbox1);
-        final boolean hasWhippedCream=checkBox.isChecked();
+        CheckBox checkBox1=(CheckBox)findViewById(R.id.whippedcreamcheckbox);
+        final boolean hasWhippedCream=checkBox1.isChecked();
+        CheckBox checkBox2=(CheckBox)findViewById(R.id.checkboxcheckbox);
+        final boolean hasChocolate=checkBox2.isChecked();
         Log.v("MainActivity","has Whipped Cream"+hasWhippedCream);
         int price= calculationPrice();
-        String priceMessage=createOrderSummary(price,hasWhippedCream);
+        String priceMessage=createOrderSummary(price,hasWhippedCream,hasChocolate);
         displayMessage(priceMessage);
     }
 
@@ -32,10 +34,18 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(price);
 
     }
-
-    public String createOrderSummary(int price, boolean addWhippedCream) {
+    /**
+     * Create summary of the order.
+     *
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate is whether or not the user wants whipped cream topping
+     * @param price of the order
+     * @return text summary
+     */
+    public String createOrderSummary(int price, boolean addWhippedCream,boolean addChocolate) {
         String priceMessage = " Name:Lyla the Labyrinth";
         priceMessage += "\n Add Whipped cream?" + addWhippedCream;
+        priceMessage += "\n Add Whipped cream?" + addChocolate;
         priceMessage += "\n Quantity:" + quantity;
         priceMessage += "\n Total:" + calculationPrice();
         priceMessage += "\n Thank you";
